7a086b8800d1a88427438cd4200ee5f1fafd2309
Nguyen Van Vu <Vux@Nguyens-MacBook-Pro.local>
2026-06-05 23:48:11 +0700

    Loading File

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path, { dirname } from 'path';
+
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+// Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = http.createServer(async (req, res) => {
 
     try{
         // check if === 'GET'
         if(req.method === 'GET'){
+            let filePath;
             if(req.url === '/'){
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(`<h1 /> Homepage`);
+                filePath = path.join(__dirname, 'public', 'index.html')
             } else if(req.url === '/about'){
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(`<h1 /> About`);
+                filePath = path.join(__dirname, 'public', 'about.html')
             } else {
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                res.end(`<h1 /> Not Found`);
+            throw new Error('Not Found');
             } 
+
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
         } else{
             throw new Error('Method is not allows');
         }
